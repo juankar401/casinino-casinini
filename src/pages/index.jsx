43d8a6043472312c04cfae506c089621ec09bd91
Gabriel Boiza <gabrielboiza2@gmail.com>
2025-12-04 23:09:55 +0100

    asdf

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,27 +1,15 @@
 "use client";
-import { useEffect } from "react";
 
 export default function Home() {
-    useEffect(() => {
-        const script = document.createElement("script");
-        <script src="/unity-build/Build/Casinino-Casinini.loader.js"></script>
-        script.async = true;
-        document.body.appendChild(script);
-
-        return () => {
-            document.body.removeChild(script);
-        };
-    }, []);
-
-    return (
-        <div style={{width: "100%", height: "100vh"}}>
-          <iframe
-              src="/unity-build/index.html"
-              width="100%"
-              height="100%"
-              style={{ border: "none" }}
-              allowFullScreen
-          />
-        </div>
-    );
+  return (
+    <div style={{ width: "100%", height: "100vh" }}>
+      <iframe
+        src="/unity-build/index.html"
+        width="100%"
+        height="100%"
+        style={{ border: "none" }}
+        allowFullScreen
+      />
+    </div>
+  );
 }
